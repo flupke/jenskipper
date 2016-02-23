c3a9c1c7eefac6606f0a8b62aac52ee8dac77ef5
Luper Rouch <luper.rouch@gmail.com>
2016-02-23 23:06:05 +0100

    add exceptions

---
+++ jenskipper/exceptions.py
@@ -0,0 +1,11 @@
+class JenskipperError(Exception):
+
+    pass
+
+
+class CyclicDependency(JenskipperError):
+
+    def __init__(self, nodes):
+        super(CyclicDependency, self).__init__(
+                'involved nodes: %s' % ', '.join(str(n) for n in nodes))
+        self.nodes = nodes
