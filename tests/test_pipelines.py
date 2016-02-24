f0911a3fed707d0d9612714cc7124012a31aad7e
Luper Rouch <luper.rouch@gmail.com>
2016-02-24 19:31:22 +0100

    typo

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -3,6 +3,6 @@ from jenskipper import pipelines
 
 def test_format_pipe_bits():
     bits = {'A': (['B', 'C'], 'SUCCESS')}
-    assert pipelines.format_pipe_bits(bits).splitlines() == ['B > A', 'C > A']
+    assert pipelines.format_pipes_bits(bits).splitlines() == ['B > A', 'C > A']
     bits = {'C': (['B'], 'SUCCESS'), 'B': (['A'], 'FAILURE')}
-    assert pipelines.format_pipe_bits(bits).splitlines() == ['A ~> B > C']
+    assert pipelines.format_pipes_bits(bits).splitlines() == ['A ~> B > C']
