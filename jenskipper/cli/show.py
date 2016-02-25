5a06e6a253329f07974488e8eb7bc2298b19dec3
Luper Rouch <luper.rouch@gmail.com>
2016-02-25 19:14:15 +0100

    implement the show command

--- jenskipper/cli/show.py
+++ jenskipper/cli/show.py
@@ -1,11 +1,19 @@
 import click
 
+from .. import repository
 from .. import jobs
 
 
 @click.command()
 @click.argument('job_name')
-def show(job_name):
+@click.option('--jk-dir', '-d', default='.', help='Location of the jenskipper '
+              'repository (default: the current directory).')
+def show(job_name, jk_dir):
     '''
-    Show the rendered XML of JOB_NAME
+    Show the rendered XML of JOB_NAME.
     '''
+    jobs_defs = repository.get_jobs_defs(jk_dir)
+    pipelines = repository.get_pipelines(jk_dir)
+    job_def = jobs_defs[job_name]
+    pipe_info = pipelines.get(job_name, None)
+    print jobs.render_job(job_def, pipe_info, jk_dir)
