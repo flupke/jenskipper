2d87ef0b89af4d314c3741b53dc12a9be6cfe4ef
Luper Rouch <luper.rouch@gmail.com>
2016-02-27 12:08:50 +0100

    fix tests

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,4 +1,7 @@
+import pytest
+
 from jenskipper import repository
+from jenskipper import exceptions
 
 
 def test_get_current_repository(data_dir):
@@ -6,4 +9,6 @@ def test_get_current_repository(data_dir):
     from_dir = base_dir.join('subdir')
     assert repository.search_base_dir(str(from_dir), data_dir) == base_dir
     assert repository.search_base_dir(str(base_dir), data_dir) == base_dir
-    assert repository.search_base_dir(str(data_dir), data_dir) is None
+    with pytest.raises(exceptions.RepositoryNotFound) as exc:
+        assert repository.search_base_dir(str(data_dir), data_dir) is None
+        assert exc.from_dir == data_dir
