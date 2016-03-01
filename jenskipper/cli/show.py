6aa0bcaca5971bdc6cb31d556a6c330ee7cfc8e4
Luper Rouch <luper.rouch@gmail.com>
2016-03-01 21:29:47 +0100

    use repository.check_dir_is_in_repository() in show command

--- jenskipper/cli/show.py
+++ jenskipper/cli/show.py
@@ -12,8 +12,9 @@ def show(job_name, jk_dir):
     '''
     Show the rendered XML of JOB_NAME.
     '''
-    jobs_defs = repository.get_jobs_defs(jk_dir)
-    pipelines = repository.get_pipelines(jk_dir)
+    base_dir = repository.check_dir_is_in_repository(jk_dir)
+    jobs_defs = repository.get_jobs_defs(base_dir)
+    pipelines = repository.get_pipelines(base_dir)
     job_def = jobs_defs[job_name]
     pipe_info = pipelines.get(job_name)
-    print jobs.render_job(job_def, pipe_info, jk_dir)
+    print jobs.render_job(job_def, pipe_info, base_dir)
