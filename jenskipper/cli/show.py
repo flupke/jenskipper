9a3c4eb06814c65d3f44e944e0044d58814bf380
Luper Rouch <luper.rouch@gmail.com>
2016-03-10 22:09:14 +0100

    add --context to the show command

--- jenskipper/cli/show.py
+++ jenskipper/cli/show.py
@@ -7,9 +7,11 @@ from .. import repository
 @click.command()
 @click.argument('job_name')
 @decorators.repos_command
+@decorators.context_command
 @decorators.handle_conf_errors
-def show(job_name, base_dir):
+def show(job_name, base_dir, context_overrides):
     '''
     Show the rendered XML of JOB_NAME.
     '''
-    print repository.get_job_conf(base_dir, job_name)
+    print repository.get_job_conf(base_dir, job_name,
+                                  context_overrides=context_overrides)
