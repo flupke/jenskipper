1fb788dae8cd3a4647354765c4fe4a3b9989678f
Luper Rouch <luper.rouch@gmail.com>
2016-03-24 18:24:14 +0100

    add functions to pretty-print jinja errors

--- jenskipper/templates.py
+++ jenskipper/templates.py
@@ -1,4 +1,7 @@
+import traceback
+
 import jinja2
+import click
 
 from . import utils
 
@@ -22,3 +25,43 @@ def render(templates_dir, template, context, context_overrides={}):
     template = env.get_template(template)
     context = utils.deep_merge(context, context_overrides)
     return template.render(**context)
+
+
+def extract_jinja_error(exc_info):
+    '''
+    Extract relevant informations from a Jinja2 exception.
+
+    *exc_info* should be a ``(exc_type, exc_value, traceback)`` tuple, as
+    returned by :func:`sys.exc_info`.
+
+    Return a ``(title, stack, error)`` tuple, where *title* is the error title,
+    *stack* a list of lines describing the stack that led to the error, and
+    *error* the description of the error.
+
+    Raise a :class:`TypeError` if the error is not a supported Jinja2 error.
+    '''
+    exc_type, exc_value, tb = exc_info
+    if exc_type is jinja2.UndefinedError:
+        title = u'Undefined variable encountered in template'
+    elif exc_type is jinja2.TemplateSyntaxError:
+        title = u'Syntax error in template'
+    else:
+        raise TypeError(exc_type)
+
+    stack_lines = []
+    jinja_tb = [x for x in traceback.extract_tb(tb)
+                if x[2] in ('top-level template code', 'template')]
+    for file_name, line, func_name, text in jinja_tb:
+        stack_lines.append(u'  File "%s", line %s' % (file_name, line))
+        stack_lines.append(u'    %s' % text)
+
+    return title, stack_lines, unicode(exc_value)
+
+
+def print_jinja_error(title, stack_lines, error):
+    click.secho(title, fg='red', bold=True)
+    click.secho('')
+    for line in stack_lines:
+        click.secho(line)
+    click.secho('')
+    click.secho(error, bold=True)
--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,3 +1,7 @@
+import sys
+
+import jinja2
+
 from jenskipper import templates
 
 
@@ -13,3 +17,34 @@ def test_render_with_overrides(data_dir):
                                 {'name': 'John'},
                                 context_overrides={'name': 'Jane'})
     assert rendered == 'My name is Jane'
+
+
+def test_extract_jinja_error_undefined_variable(data_dir):
+    tpl_name = 'template.txt'
+    tpl_path = data_dir.join(tpl_name)
+    try:
+        templates.render(unicode(data_dir), tpl_name, {})
+    except jinja2.UndefinedError:
+        title, stack, error = templates.extract_jinja_error(sys.exc_info())
+    assert title == 'Undefined variable encountered in template'
+    assert stack == [
+        '  File "%s", line 1' % tpl_path,
+        '    My name is {{ name }}',
+    ]
+    assert error == "'name' is undefined"
+
+
+def test_extract_jinja_error_syntax_error(data_dir):
+    tpl_name = 'template_with_syntax_error.txt'
+    try:
+        templates.render(unicode(data_dir), tpl_name, {})
+    except jinja2.TemplateSyntaxError:
+        title, stack, error = templates.extract_jinja_error(sys.exc_info())
+    tpl_path = data_dir.join(tpl_name)
+    assert title == 'Syntax error in template'
+    assert stack == [
+        '  File "%s", line 1' % tpl_path,
+        '    {{ foo',
+    ]
+    assert error == \
+        "unexpected end of template, expected 'end of print statement'."
