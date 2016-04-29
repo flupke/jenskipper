a7733e6d3bdde569c919ab3a55d91527890eddf1
Luper Rouch <luper.rouch@gmail.com>
2016-04-29 15:51:15 +0200

    `jk diff` return code can be used to check if the repository and server are in sync

--- jenskipper/cli/diff.py
+++ jenskipper/cli/diff.py
@@ -27,25 +27,46 @@ def diff(jobs_names, base_dir, context_overrides):
     Show diffs between JOBS in the local repository and on the server.
 
     If no JOBS are specified, diff all jobs.
+
+    Return codes:
+
+        0: the repository and the server are in sync
+
+        1: repository and server are not in sync
+
+        2: a job is present in the repository but not on the server
     '''
     if not HAVE_LXML:
         click.secho('This command works better if you install lxml:',
                     fg='yellow', bold=True)
         click.secho('  $ pip install lxml', fg='green')
     jenkins_url = conf.get(base_dir, ['server', 'location'])
+    ret_code = 0
     for job_name in jobs_names:
         try:
-            print_job_diff(base_dir, jenkins_url, job_name, context_overrides)
+            ret_code |= print_job_diff(base_dir, jenkins_url, job_name,
+                                       context_overrides)
         except exceptions.JobNotFound:
             utils.sechowrap('')
             utils.sechowrap('Unknown job: %s' % job_name, fg='red', bold=True)
             utils.sechowrap('Job is present in the local repository, but not '
                             'on the Jenkins server.', fg='red')
-            sys.exit(1)
+            sys.exit(2)
+    sys.exit(ret_code)
 
 
 def print_job_diff(base_dir, jenkins_url, job_name, context_overrides,
                    reverse=False):
+    '''
+    Print the diff between job *job_name* in the repository at *base_dir* and
+    the server at *jenkins_url*.
+
+    *context_overrides* must be a dict of values overriding the local job
+    context.
+
+    The diff shows what would change if the local job would be pushed to the
+    server, unless *reverse* is true.
+    '''
     local_xml = repository.get_job_conf(base_dir, job_name, context_overrides)
     local_xml = _prepare_xml(local_xml)
     remote_xml, _ = jenkins_api.handle_auth(base_dir,
@@ -63,6 +84,7 @@ def print_job_diff(base_dir, jenkins_url, job_name, context_overrides,
     diff = difflib.unified_diff(from_text, to_text, fromfile=from_file,
                                 tofile=to_file)
     _print_diff(diff)
+    return max(len(diff), 1)
 
 
 def _prepare_xml(xml):
