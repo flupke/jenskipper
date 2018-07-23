e85ddb9867a4f3edd27c548e4beda9004e6496fa
Luper Rouch <luper.rouch@gmail.com>
2018-07-23 10:23:42 +0200

    Fix py.path.local coersion to strings on Python 2

--- tests/cli/test_dirty.py
+++ tests/cli/test_dirty.py
@@ -7,12 +7,12 @@ from jenskipper import utils
 
 def test_dirty_git(data_dir, tmp_dir):
     repos_dir = tmp_dir.join('repos')
-    shutil.copytree(data_dir.join('repos'), repos_dir)
-    with utils.cd(repos_dir):
+    shutil.copytree(str(data_dir.join('repos')), str(repos_dir))
+    with utils.cd(str(repos_dir)):
         subprocess.check_call(['git', 'init'])
         subprocess.check_call(['git', 'add', '.'])
         subprocess.check_call(['git', 'commit', '-m', 'first commit'])
-    assert dirty.get_dirty_jobs(repos_dir, ['basic']) == set()
-    with open(repos_dir.join('templates', 'basic.txt'), 'a') as fp:
+    assert dirty.get_dirty_jobs(str(repos_dir), ['basic']) == set()
+    with open(str(repos_dir.join('templates', 'basic.txt')), 'a') as fp:
         fp.write('bar')
-    assert dirty.get_dirty_jobs(repos_dir, ['basic']) == {'basic'}
+    assert dirty.get_dirty_jobs(str(repos_dir), ['basic']) == {'basic'}
