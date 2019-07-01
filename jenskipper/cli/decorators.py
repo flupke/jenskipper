6acf849b8dfeccd9ab02f2690d6f9e8293a4a1a1
Luper Rouch <luper.rouch@gmail.com>
2019-07-01 16:23:25 +0200

    Jenskipper repo dir can be set with the JK_DIR env var

--- jenskipper/cli/decorators.py
+++ jenskipper/cli/decorators.py
@@ -23,8 +23,9 @@ def repos_command(func):
     Base options for repository commands.
     """
 
-    @click.option('--jk-dir', '-D', default='.', help='Location of the '
-                  'jenskipper repository (default: the current directory).')
+    @click.option('--jk-dir', '-D', envvar='JK_DIR', default='.',
+                  help='Location of the jenskipper repository (default: the '
+                  'current directory).')
     @functools.wraps(func)
     def wrapper(jk_dir, **kwargs):
         base_dir = repository.check_dir_is_in_repository(jk_dir)
