c2b61d5b092762e0f36a9b7d9e85ee3156a60480
Luper Rouch <luper.rouch@gmail.com>
2019-07-03 11:54:41 +0200

    tests: remove "basic" job

--- tests/cli/test_dirty.py
+++ tests/cli/test_dirty.py
@@ -1,4 +1,3 @@
-import shutil
 import subprocess
 
 from jenskipper.cli import dirty
@@ -7,12 +6,12 @@ from jenskipper import utils
 
 def test_dirty_git(data_dir, tmp_dir):
     repos_dir = tmp_dir.join('repos')
-    shutil.copytree(str(data_dir.join('repos')), str(repos_dir))
+    data_dir.join('repos').copy(repos_dir)
     with utils.cd(str(repos_dir)):
         subprocess.check_call(['git', 'init'])
         subprocess.check_call(['git', 'add', '.'])
         subprocess.check_call(['git', 'commit', '-m', 'first commit'])
-    assert dirty.get_dirty_jobs(str(repos_dir), ['basic']) == set()
-    with open(str(repos_dir.join('templates', 'basic.txt')), 'a') as fp:
-        fp.write('bar')
-    assert dirty.get_dirty_jobs(str(repos_dir), ['basic']) == {'basic'}
+    assert dirty.get_dirty_jobs(str(repos_dir), ['default_job']) == set()
+    repos_dir.join('templates', 'default_job.txt').write('bar', mode='a')
+    assert dirty.get_dirty_jobs(str(repos_dir), ['default_job']) == \
+        {'default_job'}
