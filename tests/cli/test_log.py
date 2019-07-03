4d617bc733d1a68a25cdda553a5e3234a5ab0c79
Luper Rouch <luper.rouch@gmail.com>
2019-07-03 03:46:28 +0200

    cli tests: add log tests

---
+++ tests/cli/test_log.py
@@ -0,0 +1,17 @@
+from jenskipper.cli import log
+
+
+def test_log(requests_mock):
+    requests_mock.get('/job/default_job/api/json',
+                      json={'builds': [{'url': '/foo', 'number': 1}]})
+    requests_mock.get('/foo/api/json', json={'result': 'SUCCESS'})
+    requests_mock.get('/consoleText', text='log')
+    log.log(['default_job', '--all'], standalone_mode=False)
+
+
+def test_log_with_build_number(requests_mock):
+    requests_mock.get('/job/default_job/api/json',
+                      json={'builds': [{'url': '/foo', 'number': 1}]})
+    requests_mock.get('/foo/api/json', json={'result': 'SUCCESS'})
+    requests_mock.get('/consoleText', text='log')
+    log.log(['default_job', '--all', '--build', '1'], standalone_mode=False)
